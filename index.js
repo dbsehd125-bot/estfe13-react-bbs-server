cf4d17c3efc00b9c7a6b9e3d63ea150084a910ee
dbsehd125 <dbsehd125@gmail.com>
2026-07-08 13:11:42 +0900

    user

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ const upload = multer({ storage: storage });
 
 const db = mysql.createConnection({
   host: "localhost",
-  user: "root",
+  user: "user_bbs",
   password: "1234",
   database: "bbs",
 });
